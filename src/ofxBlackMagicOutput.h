fedd2ac8b8b2dda97b2c4d7d71b8b6a059eb83f9
Frederick Rodrigues <fredaudio@gmail.com>
2015-03-04 14:07:34 +0100

    Output
    First attempt to add video output for Bm devices.

---
+++ src/ofxBlackMagicOutput.h
@@ -0,0 +1,60 @@
+#pragma once
+
+
+#include "DeckLinkAPI.h"
+#include "ofMain.h"
+#include "OpenGL/OpenGL.h"
+
+
+class ofxBlackMagicOutput : ofThread
+{
+public:
+    ofxBlackMagicOutput();
+    ~ofxBlackMagicOutput();
+    bool setup();
+    
+    void threadedFunction();
+    bool exitThreadFlag;
+    
+    void externLock(){lock();}
+    void externUnlock(){unlock();}
+    
+    
+    float getFramePosition();
+    void renderFrame(unsigned char* bytes, int length);
+    
+    
+private:
+    
+    GLenum				glStatus;
+    GLuint				idFrameBuf, idColorBuf, idDepthBuf;
+    
+    // DeckLink
+    uint32_t					uiFrameWidth;
+    uint32_t					uiFrameHeight;
+    
+    IDeckLink*					pDL;
+public:
+    IDeckLinkOutput*			pDLOutput;
+private:
+    IDeckLinkMutableVideoFrame*	pDLVideoFrame;
+    
+    BMDTimeValue				frameDuration;
+    BMDTimeScale				frameTimescale;
+    uint32_t					uiFPS;
+    uint32_t					uiTotalFrames;
+    
+    void ResetFrame();
+    
+protected:
+    
+private:
+    
+    CGLContextObj		contextObj;
+    CGLContextObj		old_contextObj;
+    
+    ofFbo fbo;
+    ofPixels pix;
+
+    
+};
